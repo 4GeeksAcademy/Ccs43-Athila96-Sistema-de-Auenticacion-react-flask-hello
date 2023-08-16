eb1b26f4c6487d787130a88796d5133bfdf50eb1
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-16 21:49:23 +0000

    Se establecieron los endpoint necesarios para crear usuarios, generar token y validar al usaurio

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,82 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+
+# ENDPOINT PARA CREAR USURIO
+
+@api.route('/singup', methods=["POST"])
+def create_user():
+
+    body =  request.json
+    email = body.get("email")
+    password = body.get("password")
+    
+    user_exist = User.query.filter_by(email=email).one_or_none()
+    if user_exist is not None:
+        return jsonify({
+            "message": "User already exists"
+        }), 400
+    
+    new_user = User(
+        email = email,
+        password = password
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "an internal error occurred",
+            "error": error.args
+        }), 500
+    
+    return jsonify({}), 201
+
+
+# ENDPOINT PARA EL LOGIN (GENERAR EL TOKEN)
+
+@api.route('/login', methods=["POST"])
+def handle_login():
+    body =  request.json
+    email = body.get("email")
+    password = body.get("password")
+
+    if email is None or password is None:
+        return jsonify({
+            "message": "email and password required"
+        }), 400
+    
+    user = User.query.filter_by(email=email).one_or_none()
+
+    if user is None:
+        return jsonify({
+            "message": "Invalid credential"
+        }), 400
+    
+    password_valid = password == user.password
+
+    if not password_valid:
+        return jsonify({
+           "message": "email and password required"
+        }), 400
+    
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify({
+        "token": access_token 
+    }), 201
+
+
+# 
+
+@api.route('/private', methods=["GET"])
+@jwt_required()
+def get_user():
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
+    return jsonify(user.serialize()), 200
